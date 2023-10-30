c10ed066dad38b4a2a16352a394d0b658ba4e797
arpit30699 <arpit30699@gmail.com>
2023-10-30 13:12:16 +0530

    added face

---
+++ face.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+# Load the pre-trained face detection model
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+# Function to detect faces in the video stream
+def detect_faces(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    return frame
+
+# Function to generate video frames
+def generate_frames():
+    cap = cv2.VideoCapture(0)  # Use the default camera (you can change the argument to use a different camera)
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frame = detect_faces(frame)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if ret:
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(debug=True)
